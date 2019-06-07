2c328528a2cfeb8d8b04891aa0ce9391ef6ab4be
Yok chainarong <31986505+chainarongyok@users.noreply.github.com>
2019-06-07 10:53:33 +0700

    Delete testcalculate.sh

--- testcalculate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-read -p "Enter your first number: " FIRSTNUMBER
-read -p "Enter your seccond number: " SECCONDNUMBER
-
-$SUM=$(($FIRSTNUMBER + $SECCONDNUMBER))
-echo -e "Total value is $SUM" 
