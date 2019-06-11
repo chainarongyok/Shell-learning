7d8d92df6a104216fdbfc3ee3a306f2243b5ff08
admin <git@github.com>
2019-06-11 09:19:08 +0700

    Add file calculaterpython.py

---
+++ calculaterpython.py
@@ -0,0 +1,17 @@
+num1 = 10
+num2 = 20
+
+sum = num1 + num2
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+sum1 = num1 - num2
+print('The sum of {0} and {1} is {2}'.format(num1,num2,sum1))
+
+sum2 = num1 * num2
+print('The sum of {0} and {1} is {2}'.format(num1,num2,sum2))
+
+sum3 = num1 / num2
+print('The sum of {0} and {1} is {2}'.format(num1,num2,sum3))
+
+sum4 = num1 % num2
+print('The sum of {0} and {1} is {2}'.format(num1,num2,sum3))
